8eea7dcec4b0851670e8f4b7ffab18ae9de513ec
ramon-fields <ramonfields87@gmail.com>
2020-09-11 20:28:59 -0400

    First submission cookie orders

--- src/main/java/cookies/CookieOrder.java
+++ src/main/java/cookies/CookieOrder.java
@@ -1,4 +1,19 @@
 package cookies;
 
 public class CookieOrder {
+    //Fields
+    private int numBoxes;
+    private String variety;
+    //Constructor
+    public CookieOrder(String localVariety, int localNumBoxes) {
+        this.numBoxes = localNumBoxes;
+        this.variety = localVariety;
+    }
+    //Accessors
+    public int getNumBoxes() {
+        return this.numBoxes;
+    }
+    public String getVariety() {
+        return this.variety;
+    }
 }
